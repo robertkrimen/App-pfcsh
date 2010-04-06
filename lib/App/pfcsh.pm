2aa19e76d917d631596d2aff022088ed0bfda927
robertkrimen <robertkrimen@gmail.com>
2010-04-05 18:17:40 -0700

    Dist::Zilla mangling

---
+++ lib/App/pfcsh.pm
@@ -0,0 +1,17 @@
+package App::pfcsh;
+# ABSTRACT: Compile Flex & Air applications from a persistent commandline
+
+use strict;
+use warnings;
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+App::pfcsh
+
+=cut 
